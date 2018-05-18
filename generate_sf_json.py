b5c68852bc7fed85c69582752b639b3689b17270
Jacob Joaquin <jacobjoaquin@gmail.com>
2018-05-17 20:01:16 -0700

    Updates JSON file, doesn't overwrite

--- generate_sf_json.py
+++ generate_sf_json.py
@@ -4,10 +4,17 @@ import json
 
 print('Generating Soundfile JSON file: soundsfiles.json')
 
+filename = 'soundfiles.json'
 file_path = './assets/'
 files = os.listdir(file_path)
 data = {}
+file_data = {}
 
+# Load existing
+with open(filename) as f:
+    data = json.load(f)
+
+# Get data from files
 for name in files:
     path_name = file_path + name
     sf = 0
@@ -26,9 +33,16 @@ for name in files:
             'duration': duration
         }
 
-        data[name] = info
+        file_data[name] = info
     except:
         pass
 
-with open('soundfiles.json', 'w') as f:
+# Update existing
+for k in file_data.keys():
+    # Update from file
+    for k2, v in file_data[k].items():
+        data[k][k2] = v
+
+# Save
+with open(filename, 'w') as f:
     json.dump(data, f, indent=4, sort_keys=True)
